56e5439b4dea93c47ba9a7be55b792d7e5ad9ae0
Kilowon <shauncameron.inbox@gmail.com>
2022-11-18 16:37:51 -0500

    update - state objects for props

--- ReactMp3Project/src/App.jsx
+++ ReactMp3Project/src/App.jsx
@@ -1,12 +1,22 @@
 import './styles/App.scss'
 import Player from './components/Player'
 import Song from './/components/Song'
+import { useState } from 'react'
+import chillHop from './util.jsx'
 
 function App() {
+	const [songs, setSongs] = useState(chillHop())
+	const [currentSong, setCurrentSong] = useState(songs[0])
+	const [isPlaying, setIsPlaying] = useState(false)
 	return (
 		<div className="App">
-			<Song />
-			<Player />
+			<Song currentSong={currentSong} />
+			<Player
+				currentSong={currentSong}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+			/>
 		</div>
 	)
 }
