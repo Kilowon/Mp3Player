d62ba354250d9d1137ad232b95efeaa19f0263df
Kilowon <shauncameron.inbox@gmail.com>
2022-11-18 16:37:25 -0500

    create - play functionality

--- ReactMp3Project/src/components/Player.jsx
+++ ReactMp3Project/src/components/Player.jsx
@@ -2,10 +2,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
 	faPlay,
 	faAngleLeft,
-	faAngleRight
+	faAngleRight,
+	faPause
 } from '@fortawesome/free-solid-svg-icons'
+import { useRef, useState } from 'react'
 
-const Player = () => {
+const Player = ({ currentSong, setCurrentSong, isPlaying, setIsPlaying }) => {
+	// Refs
+	const audioRef = useRef(null)
+	const playSongHandler = () => {
+		if (isPlaying) {
+			audioRef.current.pause()
+			setIsPlaying(!isPlaying)
+		} else {
+			audioRef.current.play()
+			setIsPlaying(!isPlaying)
+		}
+	}
+	const timeUpdateHandler = e => {
+		const current = e.target.currentTime
+		const duration = e.target.duration
+		setSongInfo({ ...songInfo, currentTime: current, duration })
+	}
+	const [songInfo, setSongInfo] = useState({
+		currentTime: null,
+		duration: null
+	})
+
 	return (
 		<div className="player">
 			<div className="time-control">
@@ -15,13 +38,19 @@ const Player = () => {
 			</div>
 			<div className="play-control">
 				<FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-				<FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+				<FontAwesomeIcon
+					onClick={playSongHandler}
+					className="play"
+					size="2x"
+					icon={faPlay}
+				/>
 				<FontAwesomeIcon
 					className="skip-forward"
 					size="2x"
 					icon={faAngleRight}
 				/>
 			</div>
+			<audio ref={audioRef} src={currentSong.audio}></audio>
 		</div>
 	)
 }
