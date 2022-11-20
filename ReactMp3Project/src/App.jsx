35e751ba3ed3eb45c6f071a5280df69494bec4d9
Kilowon <shauncameron.inbox@gmail.com>
2022-11-20 15:09:39 -0500

    add - new components

--- ReactMp3Project/src/App.jsx
+++ ReactMp3Project/src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/App.scss'
 import Player from './components/Player'
 import Song from './/components/Song'
+import Library from './components/Library'
 import { useState } from 'react'
 import chillHop from './util.jsx'
 
@@ -8,6 +9,7 @@ function App() {
 	const [songs, setSongs] = useState(chillHop())
 	const [currentSong, setCurrentSong] = useState(songs[0])
 	const [isPlaying, setIsPlaying] = useState(false)
+
 	return (
 		<div className="App">
 			<Song currentSong={currentSong} />
@@ -15,6 +17,11 @@ function App() {
 				currentSong={currentSong}
 				setCurrentSong={setCurrentSong}
 				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+			/>
+			<Library
+				songs={songs}
+				setCurrentSong={setCurrentSong}
 				setIsPlaying={setIsPlaying}
 			/>
 		</div>
