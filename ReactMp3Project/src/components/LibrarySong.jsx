fe1022ecff984394406ad59b59a0a30c9fb68cb2
Kilowon <shauncameron.inbox@gmail.com>
2022-11-20 15:09:22 -0500

    new - component

---
+++ ReactMp3Project/src/components/LibrarySong.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LibrarySong = ({ id, song, songs, setCurrentSong, setIsPlaying }) => {
+	const songSelectHandler = async () => {
+		await setCurrentSong(song)
+		setIsPlaying(true)
+		/* if (songInfo.currentTime === 0) {
+			audioRef.current.volume = 0.1
+		} */
+	}
+
+	return (
+		<div onClick={songSelectHandler} className="library-song">
+			<img src={song.cover} />
+			<div className="song-description">
+				<h3>{song.name}</h3>
+				<h4>{song.artist}</h4>
+			</div>
+		</div>
+	)
+}
+
+export default LibrarySong
