277b9f6fb56298e243d15cebf34f0b787f49d6bb
Kilowon <shauncameron.inbox@gmail.com>
2022-11-22 19:42:19 -0500

    added - playlist items

--- ReactMp3Project/src/util.jsx
+++ ReactMp3Project/src/util.jsx
@@ -81,6 +81,86 @@ function chillHop() {
 			color: ['#BC8D7D', '#CBA65F'],
 			id: uuidv4(),
 			active: false
+		},
+		{
+			name: 'Little Bird',
+			cover:
+				'https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/61226ba21704e25a2ece5728_tet0c8gdl30scqu-p-1080.png',
+			artist: 'Liphe',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=21735',
+			color: ['#829EA8', '#EF977C'],
+			id: uuidv4(),
+			active: true
+		},
+		{
+			name: 'Cabaña',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg',
+			artist: 'Juan Rios',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=21647',
+			color: ['#FFC595', '#FDCA95'],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Chit Chat',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/07/5821e04fd52fa668a0b9890f213cbb12e454cc6b-1024x1024.jpg',
+			artist: 'Middle School',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=22934',
+			color: ['#D66D40', '#3E515D'],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'travelbag',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg',
+			artist: 'mommy, Sleepy Fish',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=19058&',
+			color: ['', ''],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Now',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg',
+			artist: 'Misha, Jussi Halme',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=15080&',
+			color: ['#5A8ECE', '#84A9E0'],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'By Chance',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg',
+			artist: 'SwuM',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=15224',
+			color: ['#243230', '#905D26'],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Desire',
+			cover:
+				'https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/602bd6305eeaa21fc1a6db5e_cover1a.png',
+			artist: 'Psalm Trees, Guillaume Muschalle',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=10554',
+			color: ['#F4DC68', '#A57937'],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Doin It',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/07/97781364b9aa2c2896642b5f896761e07fd9894e-1024x1024.jpg',
+			artist: 'Tesk',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=8927',
+			color: ['#BC8D7D', '#CBA65F'],
+			id: uuidv4(),
+			active: false
 		}
 	]
 }
