86edd35bea652f7de6c82eccd0ad29491be9ed2c
Kilowon <shauncameron.inbox@gmail.com>
2022-11-23 08:18:20 -0500

    updates to menu

--- ReactMp3Project/src/components/Library.jsx
+++ ReactMp3Project/src/components/Library.jsx
@@ -10,7 +10,6 @@ const Library = ({
 }) => {
 	return (
 		<div className={`library ${libraryStatus ? 'active-library' : ''} `}>
-			<h2>Library</h2>
 			<div className="library-songs">
 				{songs.map(song => (
 					<LibrarySong
