093cddfd78d7b2ade07ef701acf688f9137f362f
Kilowon <shauncameron.inbox@gmail.com>
2022-11-23 08:20:12 -0500

    cleanup

--- ReactMp3Project/src/components/Song.jsx
+++ ReactMp3Project/src/components/Song.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 
 const Song = ({ currentSong, isPlaying }) => {
 	return (
-		<div
-			className={`song-container ${isPlaying ? 'bg-pan-bottom container' : ''}`}
-		>
-			<img
-				src={currentSong.cover}
-				className={` ${isPlaying ? 'rotate-center' : ''} `}
-			/>
-			<h2>{currentSong.name}</h2>
-			<h3>{currentSong.artist}</h3>
-			{[...Array(100)].map((_, i) => (
-				<div className="circle-container" key={i}>
-					<div className="circle"></div>
-				</div>
-			))}
+		<div>
+			<div
+				className={`song-container ${
+					isPlaying ? 'bg-pan-bottom container' : ''
+				}`}
+			>
+				<img
+					src={currentSong.cover}
+					className={` ${isPlaying ? 'rotate-center' : ''} `}
+				/>
+				<h2>{currentSong.name}</h2>
+				<h3>{currentSong.artist}</h3>
+			</div>
+			{/* <div className="bob">
+				{[...Array(100)].map((_, i) => (
+					<div className="circle-container" key={i}>
+						<div className="circle"></div>
+					</div>
+				))}
+			</div> */}
 		</div>
 	)
 }
